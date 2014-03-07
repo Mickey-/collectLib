3eb824ea2ced754aef3b3c442e8792b6ec06e738
Mickey- <itaofe@gmail.com>
2014-03-07 13:34:14 +0800

    add readme, modify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,36 @@ var LIBS = Object.keys(require('./bower.json').dependencies),
 
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require("gulp-rename"),
-    colors = require("colors"),
+    rename = require('gulp-rename'),
+    colors = require('colors'),
+    exec = require('child_process').exec,
+    sys = require('sys'),
     finder = require('fs-finder');
 
 gulp.task('default', function() {
+    //bower.commands.install(LIBS, {save: true});
+    exec("bower install", extract);
     //循环体v为各个库文件名，如“jquery”，“underscore”
-    LIBS.forEach(function(v, k){
-        var ff = finder.from(root + v),
-            minFileName = ff.findFile(v + '<.>min.js'),
-            fullFileName,
-            rs;
-        if (minFileName) {
-            rs = gulp.src(minFileName);
-        } else {
-            fullFileName = ff.findFile(v + '.js');
-            if (fullFileName) {
-                rs = gulp.src(fullFileName).pipe(uglify()).pipe(rename(v + '.min.js'));
+    function extract (error, stdout, stderr){
+        sys.puts(stdout);
+        LIBS.forEach(function(v, k){
+            var ff = finder.from(root + v),
+                minFileName = ff.findFile(v + '<.>min.js'),
+                fullFileName,
+                rs;
+            if (minFileName) {
+                rs = gulp.src(minFileName);
             } else {
-                console.log('该文件在包中命名不规则，请手动复制到build/目录中 ------ ' + (v).red);
-                return; 
+                fullFileName = ff.findFile(v + '.js<$>');
+                if (fullFileName) {
+                    rs = gulp.src(fullFileName).pipe(uglify()).pipe(rename(v + '.min.js'));
+                } else {
+                    console.log('该文件在包中命名不规则，请手动复制到build/目录中 ------ ' + (v).red);
+                    return; 
+                }
             }
-        }
-        rs.pipe(gulp.dest('./build/'));
-        console.log(' lib file has generated in build/ ------ ' + (v + '.min.js').green);
-    });
+            rs.pipe(gulp.dest('./build/'));
+            console.log(' lib file has generated in build/ ------ ' + (v + '.min.js').green);
+        });
+    }
 })
