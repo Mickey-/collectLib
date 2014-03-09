2860f4df4ecf9576f2a35df4a1dc5671cf1e65de
Mickey- <itaofe@gmail.com>
2014-03-09 14:47:16 +0800

    modify gulp and update readme

--- gulpfile.js
+++ gulpfile.js
@@ -18,21 +18,22 @@ gulp.task('default', function() {
         LIBS.forEach(function(v, k){
             var ff = finder.from('./build/' + v),
                 minFilePath = ff.findFile(v + '.min.js'),
+                fileName,
                 fullFilePath,
                 rs;
-                console.log('min'.yellow + minFilePath)
+            //如果有以.min.js结尾的文件则表示bower包完整，结束;反正处理。
             if (!minFilePath) {
-                //判断v是否是其他版本如jquery-2.1.0,如果是则剔除版本号
-                fullFilePath = ff.findFile(v.replace(/-\d\.\d\.\d$/, '') + '.js<$>');
-                console.log('full'.blue + fullFilePath)
+                //判断v是否是其他版本如jquery-2.1.0,如果是则剔除版本号以寻找文件名。
+                fileName = v.replace(/-\d\.\d\.\d$/, '') + '.js';
+                fullFilePath = ff.findFile(fileName + '<$>');
                 if (fullFilePath) {
                     rs = gulp.src(fullFilePath).pipe(uglify()).pipe(rename(v + '.min.js'));
                 } else {
                     console.log('该文件在包中命名不规则，请手动复制到build/目录中 ------ ' + (v).red);
-                    return; 
+                    return;
                 }
                 //rs.pipe(gulp.dest('./build/'));
-                rs.pipe(gulp.dest(fullFilePath.replace(v + '.js', '')));
+                rs.pipe(gulp.dest(fullFilePath.replace(new RegExp(fileName + '$'), '')));
             }
             console.log(' lib file has generated in build/ ------ ' + (v + '.min.js').green);
         });
