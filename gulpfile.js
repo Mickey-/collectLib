72cce10e9ce36a1b073cc2d987c23532bb2bcdd7
Mickey- <itaofe@gmail.com>
2014-03-10 17:16:41 +0800

    update gulp to adapt new type js,import requirejs

--- gulpfile.js
+++ gulpfile.js
@@ -25,12 +25,13 @@ gulp.task('default', function() {
             //如果有以.min.js结尾的文件则表示bower包完整，结束;反正处理。
             if (!minFilePath) {
                 //判断v是否是其他版本如jquery-2.1.0,如果是则剔除版本号以寻找文件名。
-                fileName = v.replace(/-\d\.\d\.\d$/, '') + '.js';
+                //再判断是否是形如requirejs的库，注册名为requirejs，文件名为require.js而不是requirejs.js
+                fileName = v.replace(/-\d\.\d\.\d$/, '').replace(/js$/, '') + '.js';
                 fullFilePath = ff.findFile(fileName + '<$>');
                 if (fullFilePath) {
                     rs = gulp.src(fullFilePath).pipe(uglify()).pipe(rename(v + '.min.js'));
                 } else {
-                    console.log('该文件在包中命名不规则，请检查bower.json或手动复制到build/目录中 ------ ' + (v).red);
+                    console.log('该文件在包中命名不规则，请检查bower.json或手动处理到对应目录中 ------ ' + (v).red);
                     return;
                 }
                 rs.pipe(gulp.dest(fullFilePath.replace(new RegExp(fileName + '$'), '')));
